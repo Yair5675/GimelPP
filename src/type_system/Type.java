4bd2d6e169f54c4526baa1cfe512c4a3ebe3c9f0
yairz <yairziv5675@gmail.com>
2023-05-19 20:02:28 +0300

    added isPrimitive to Type.java (allows for primitive checks later)

--- src/type_system/Type.java
+++ src/type_system/Type.java
@@ -1,5 +1,10 @@
 package type_system;
 
 public abstract class Type {
+    private boolean isPrimitive;
     public abstract String getType();
+
+    public boolean isPrimitive() {
+        return this.isPrimitive;
+    }
 }
