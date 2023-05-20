41d6ff416fd6ca5418cae1017f8956ab6f6931e1
yairz <yairziv5675@gmail.com>
2023-05-20 21:21:32 +0300

    Added logical boolean operations to the Bool class

--- src/type_system/Array.java
+++ src/type_system/Array.java
@@ -2,13 +2,12 @@ package type_system;
 
 public class Array<T extends Primitive<?>> extends Primitive<T[]> {
 
-    @SafeVarargs
     public Array(T ... values) {
         this.value = values;
     }
 
     @Override
     public String getType() {
-        return null;
+        return "מערך";
     }
 }
--- src/type_system/Bool.java
+++ src/type_system/Bool.java
@@ -5,6 +5,22 @@ public class Bool extends Primitive<Boolean> {
         this.value = value;
     }
 
+    public Bool not() {
+        return new Bool(!this.value);
+    }
+
+    public Bool or(Bool other) {
+        return new Bool(this.value || other.value);
+    }
+
+    public Bool and(Bool other) {
+        return new Bool(this.value && other.value);
+    }
+
+    public Bool xor(Bool other) {
+        return new Bool(this.value ^ other.value);
+    }
+
     @Override
     public String getType() {
         return "בוליאני";
