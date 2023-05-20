e2bbf782e4e901eafa569417dc719c9a2e725c4e
yairz <yairziv5675@gmail.com>
2023-05-20 21:50:12 +0300

    Added primitive types addition for Bool and FloatingNumber

--- src/type_system/Bool.java
+++ src/type_system/Bool.java
@@ -1,8 +1,39 @@
 package type_system;
 
+import errors.ArithmeticErrors;
+
 public class Bool extends Primitive<Boolean> {
     public Bool(final boolean value) {
         this.value = value;
+    }
+
+    public Primitive<?> add(Primitive<?> other) throws ArithmeticErrors.InvalidAdditionTypesError {
+        if (other == null) return null;
+
+        // Checking for concrete types:
+        if (other instanceof Bool) return this.add((Bool) other);
+        else if (other instanceof Int) return this.add((Int) other);
+        else if (other instanceof FloatingNumber) return this.add((FloatingNumber) other);
+        else if (other instanceof Str) return this.add((Str) other);
+
+        // For any other type addition is invalid:
+        else throw new ArithmeticErrors.InvalidAdditionTypesError(this, other);
+    }
+
+    private Bool add(Bool other) {
+        return this.or(other);
+    }
+
+    private Int add(Int num) {
+        return new Int(num.value + (this.value ? 1 : 0));
+    }
+
+    private FloatingNumber add(FloatingNumber floatingNum) {
+        return new FloatingNumber(floatingNum.value + (this.value ? 1 : 0));
+    }
+
+    private Str add(Str str) {
+        return new Str(this.value + str.value);
     }
 
     public Bool not() {
--- src/type_system/FloatingNumber.java
+++ src/type_system/FloatingNumber.java
@@ -1,8 +1,39 @@
 package type_system;
 
+import errors.ArithmeticErrors;
+
 public class FloatingNumber extends Primitive<Double> {
     public FloatingNumber(final double value) {
         this.value = value;
+    }
+
+    public Primitive<?> add(Primitive<?> other) throws ArithmeticErrors.InvalidAdditionTypesError {
+        if (other == null) return null;
+
+        // Checking concrete types:
+        if (other instanceof Bool) return this.add((Bool) other);
+        else if (other instanceof Int) return this.add((Int) other);
+        else if (other instanceof FloatingNumber) return this.add((FloatingNumber) other);
+        else if (other instanceof Str) return this.add((Str) other);
+
+        // Only the types above are valid for addition with float, everything else is not:
+        else throw new ArithmeticErrors.InvalidAdditionTypesError(this, other);
+    }
+
+    private FloatingNumber add(Bool bool) {
+        return new FloatingNumber(this.value + (bool.value ? 1 : 0));
+    }
+
+    private FloatingNumber add(Int num) {
+        return new FloatingNumber(this.value + num.value);
+    }
+
+    private FloatingNumber add(FloatingNumber other) {
+        return new FloatingNumber(this.value + other.value);
+    }
+
+    private Str add(Str str) {
+        return new Str(this.value + str.value);
     }
 
     @Override
